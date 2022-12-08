bf921d5b6f1e756d9b316e17576c45f9b53c7b1b
FewNCN <108906267+FewNCN@users.noreply.github.com>
2022-12-08 12:28:35 +0100

    Update ProductContainer.java

--- src/model/ProductContainer.java
+++ src/model/ProductContainer.java
@@ -21,4 +21,16 @@ private ArrayList<Product> container;
 	public void addProduct(Product product) {
 		container.add(product);
 	}
+	
+	public void addNewProduct(String description, String location, int quantity, int minQuantity, int maxQuantity,
+			double costPrice, double retailPrice, double tradeAllowance) {
+		Product p = new Product(description, location, quantity, minQuantity, maxQuantity, costPrice, retailPrice, tradeAllowance);
+		container.add(p);
+	}
+	
+	public double 
+
+	addNewProduct("Træ 2x4m", "Timber department", 200, 50, 300, 100, 124.99, 24.99);
+	addNewProduct(, "")
+	
 }
