33a9c1c3cb2164a4627e7b1dee3ad8b013acdf83
DePlaced <117630280+DePlaced@users.noreply.github.com>
2022-12-08 14:20:46 +0100

    finders for customer

--- src/model/CustomerContainer.java
+++ src/model/CustomerContainer.java
@@ -22,5 +22,67 @@ private ArrayList<Customer> container;
 		container.add(customer);
 	}
 	
+    public Customer findCustomerByName(String name){
+        Customer customer = null;
+        int index = 0;
+        boolean found = false;
+        while(index < container.size() && !found){
+            customer = container.get(index);
+            if(customer.getName().equals(name)){
+                found = true;
+            }
+            else{
+                 index++;
+                }
+         }
+        if (found){
+            return container.get(index);
+        }
+        else{   
+        	return null;
+        }
+    }
+
+    public Customer findCustomerByPhoneNo(String phoneNo){
+        Customer customer = null;
+        int index = 0;
+        boolean found = false;
+        while(index < container.size() && !found){
+            customer = container.get(index);
+            if(customer.getPhoneNo().equals(phoneNo)){
+                found = true;
+            }
+            else{
+                 index++;
+                }
+         }
+        if (found){
+            return container.get(index);
+        }
+        else{   
+        	return null;
+        }
+    }
+    
+    public Customer findCustomerByCvr(String Cvr){
+        Customer customer = null;
+        int index = 0;
+        boolean found = false;
+        while(index < container.size() && !found){
+            customer = container.get(index);
+            if(customer.getCvr().equals(Cvr)){
+                found = true;
+            }
+            else{
+                 index++;
+                }
+         }
+        if (found){
+            return container.get(index);
+        }
+        else{   
+        	return null;
+        }
+    }
 	
 }
