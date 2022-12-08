48c7a39724cfc0d5b57f42efba0e91685025a885
DePlaced <117630280+DePlaced@users.noreply.github.com>
2022-12-08 14:28:54 +0100

    getters and setters order

--- src/model/Order.java
+++ src/model/Order.java
@@ -16,6 +16,38 @@ public class Order {
 		this.employee = employee;
 	}
 	
+	public ArrayList<PartOrder> getParts() {
+		return parts;
+	}
+
+	public void setParts(ArrayList<PartOrder> parts) {
+		this.parts = parts;
+	}
+
+	public LocalDate getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalDate dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
 	public void addPartOrder(Product product, int quantity) {
 		parts.add(new PartOrder(product, quantity));
 	}
