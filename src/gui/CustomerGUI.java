b3e3fda3ada3d9528a8ea9a7615d52ce3c34bd25
DePlaced <117630280+DePlaced@users.noreply.github.com>
2023-01-03 14:10:22 +0100

    Update CustomerGUI.java

--- src/gui/CustomerGUI.java
+++ src/gui/CustomerGUI.java
@@ -47,7 +47,7 @@ public class CustomerGUI extends JDialog {
 	private JTextField txtCity;
 	private JTextField txtCountry;
 	private JTextField txtDiscount;
-
+	private JList list;
 	/**
 	 * Launch the application.
 	 */
@@ -65,7 +65,7 @@ public class CustomerGUI extends JDialog {
 	 * Create the dialog.
 	 */
 	public CustomerGUI(Customer c) {
-		setBounds(150, 150, 675, 450);
+		setBounds(150, 150, 450, 450);
 		getContentPane().setLayout(null);
 		contentPanel.setBounds(0, 0, 434, 11);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -82,28 +82,22 @@ public class CustomerGUI extends JDialog {
 		}
 		{
 			JPanel buttonPanel = new JPanel();
-			buttonPanel.setBounds(434, 367, 215, 33);
+			buttonPanel.setBounds(344, 308, 80, 92);
 			getContentPane().add(buttonPanel);
-			{
-				btnCancel = new JButton("Cancel");
-				btnCancel.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						cancelClicked();
-					}
-				});
-				btnCancel.setActionCommand("Cancel");
-			}
-			{
-				btnSave = new JButton("Save");
-				btnSave.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						saveClicked();
-					}
-				});
-			}
-			buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+			GridBagLayout gbl_buttonPanel = new GridBagLayout();
+			gbl_buttonPanel.columnWidths = new int[]{0, 0, 0, 0, 0};
+			gbl_buttonPanel.rowHeights = new int[]{0, 0, 0, 0};
+			gbl_buttonPanel.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+			gbl_buttonPanel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
+			buttonPanel.setLayout(gbl_buttonPanel);
 			{
 				btnOK = new JButton("OK");
+				GridBagConstraints gbc_btnOK = new GridBagConstraints();
+				gbc_btnOK.fill = GridBagConstraints.HORIZONTAL;
+				gbc_btnOK.insets = new Insets(0, 0, 5, 5);
+				gbc_btnOK.gridx = 1;
+				gbc_btnOK.gridy = 0;
+				buttonPanel.add(btnOK, gbc_btnOK);
 				btnOK.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						okClicked();
@@ -112,9 +106,32 @@ public class CustomerGUI extends JDialog {
 				btnOK.setActionCommand("OK");
 				getRootPane().setDefaultButton(btnOK);
 			}
-			buttonPanel.add(btnOK);
-			buttonPanel.add(btnSave);
-			buttonPanel.add(btnCancel);
+			btnSave = new JButton("Save");
+			GridBagConstraints gbc_btnSave = new GridBagConstraints();
+			gbc_btnSave.fill = GridBagConstraints.HORIZONTAL;
+			gbc_btnSave.insets = new Insets(0, 0, 5, 5);
+			gbc_btnSave.gridx = 1;
+			gbc_btnSave.gridy = 1;
+			buttonPanel.add(btnSave, gbc_btnSave);
+			{
+				btnCancel = new JButton("Cancel");
+				GridBagConstraints gbc_btnCancel = new GridBagConstraints();
+				gbc_btnCancel.insets = new Insets(0, 0, 0, 5);
+				gbc_btnCancel.gridx = 1;
+				gbc_btnCancel.gridy = 2;
+				buttonPanel.add(btnCancel, gbc_btnCancel);
+				btnCancel.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						cancelClicked();
+					}
+				});
+				btnCancel.setActionCommand("Cancel");
+			}
+			btnSave.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					saveClicked();
+				}
+			});
 		}
 				
 		JLabel lblNewLabel_2 = new JLabel("Customer Info:");
@@ -123,31 +140,31 @@ public class CustomerGUI extends JDialog {
 		getContentPane().add(lblNewLabel_2);
 		
 		txtName = new JTextField();
-		txtName.setBounds(74, 30, 85, 20);
+		txtName.setBounds(74, 30, 270, 20);
 		getContentPane().add(txtName);
 		txtName.setColumns(10);
 		
 		txtCVR = new JTextField();
-		txtCVR.setBounds(74, 60, 85, 20);
+		txtCVR.setBounds(74, 60, 270, 20);
 		getContentPane().add(txtCVR);
 		txtCVR.setColumns(10);
 		
 		txtPhone = new JTextField();
-		txtPhone.setBounds(74, 90, 85, 20);
+		txtPhone.setBounds(74, 90, 270, 20);
 		getContentPane().add(txtPhone);
 		txtPhone.setColumns(10);
 		
 		txtEmail = new JTextField();
-		txtEmail.setBounds(74, 120, 85, 20);
+		txtEmail.setBounds(74, 120, 270, 20);
 		getContentPane().add(txtEmail);
 		txtEmail.setColumns(10);
 		
 		txtAddress = new JTextField();
-		txtAddress.setBounds(74, 150, 85, 20);
+		txtAddress.setBounds(74, 150, 270, 20);
 		getContentPane().add(txtAddress);
 		txtAddress.setColumns(10);
 		
-		JLabel name = new JLabel("Name.................");
+		JLabel name = new JLabel("Name.............");
 		name.setBounds(10, 36, 64, 15);
 		getContentPane().add(name);
 		
@@ -176,17 +193,17 @@ public class CustomerGUI extends JDialog {
 		getContentPane().add(lblNewLabel_8);
 		
 		txtPostalcode = new JTextField();
-		txtPostalcode.setBounds(74, 180, 85, 20);
+		txtPostalcode.setBounds(74, 180, 270, 20);
 		getContentPane().add(txtPostalcode);
 		txtPostalcode.setColumns(10);
 		
 		txtCity = new JTextField();
-		txtCity.setBounds(74, 210, 85, 20);
+		txtCity.setBounds(74, 210, 270, 20);
 		getContentPane().add(txtCity);
 		txtCity.setColumns(10);
 		
 		txtCountry = new JTextField();
-		txtCountry.setBounds(74, 240, 85, 20);
+		txtCountry.setBounds(74, 240, 270, 20);
 		getContentPane().add(txtCountry);
 		txtCountry.setColumns(10);
 		
@@ -195,7 +212,7 @@ public class CustomerGUI extends JDialog {
 		getContentPane().add(lblNewLabel_9);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 314, 333, 86);
+		panel.setBounds(10, 321, 334, 79);
 		getContentPane().add(panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
 		gbl_panel.columnWidths = new int[]{216, 2, 0};
@@ -212,7 +229,7 @@ public class CustomerGUI extends JDialog {
 		gbc_scrollPane.gridy = 0;
 		panel.add(scrollPane, gbc_scrollPane);
 		
-		JList list = new JList();
+		list = new JList();
 		GridBagConstraints gbc_list = new GridBagConstraints();
 		gbc_list.insets = new Insets(0, 0, 0, 5);
 		gbc_list.gridx = 0;
@@ -224,9 +241,15 @@ public class CustomerGUI extends JDialog {
 		getContentPane().add(lblNewLabel_1);
 		
 		txtDiscount = new JTextField();
-		txtDiscount.setBounds(74, 270, 85, 20);
+		txtDiscount.setBounds(74, 270, 270, 20);
 		getContentPane().add(txtDiscount);
 		txtDiscount.setColumns(10);
+		
+		JLabel lblNewLabel_10 = new JLabel("Orderlist:");
+		lblNewLabel_10.setBounds(10, 308, 64, 14);
+		getContentPane().add(lblNewLabel_10);
+		{
+		}
 		
 		init(c);
 	}
@@ -237,7 +260,6 @@ public class CustomerGUI extends JDialog {
 		
 		if(currCustomer != null) {
 			displayCustomer();
-			cCtrl.findCustomerByPhoneNo(currCustomer.getPhoneNo());
 		}
 	}
 		
