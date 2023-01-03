e22ef733100337861916d975076e32713f772303
kaer98 <56426532+kaer98@users.noreply.github.com>
2023-01-03 11:55:36 +0100

    make customerMenu

---
+++ src/gui/CustomerTM.java
@@ -0,0 +1,65 @@
+package gui;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import model.Customer;
+
+public class CustomerTM extends AbstractTableModel {
+
+	private List<Customer> data;
+	private static final String[] COL_NAMES = {"Name", "Email", "Phone", "CVR"};
+	
+	public CustomerTM() {
+		this.data = new ArrayList<>();
+	}
+	
+	
+	public CustomerTM(List<Customer> data) {
+		this.data = data;
+	    if(this.data == null) {
+	        this.data = new ArrayList<>();
+	    }
+	}
+@Override
+public int getRowCount() {
+	return data.size();
+}
+
+
+@Override
+public String getColumnName(int col) {
+    return COL_NAMES[col];
+}
+@Override
+public int getColumnCount() {
+    return COL_NAMES.length;
+}
+
+@Override
+public Object getValueAt(int rowIndex, int columnIndex) {
+	Customer c = data.get(rowIndex);
+	String res = "";
+    switch(columnIndex) {
+        case 0: res = c.getName(); break;
+        case 1: res = "" + c.getMailAddress(); break;
+        case 2: res = "" + c.getPhoneNo(); break;
+        case 3: res = "" + c.getCvr(); break;
+        default: res = "<UNKNOWN " + columnIndex + ">";
+    }
+    return res;
+
+}
+
+
+public Customer getSelectedMember(int i) {
+	return data.get(i);
+	
+}
+public void setData(List<Customer> members) {
+	
+}
+}
--- src/gui/Login.java
+++ src/gui/Login.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class login extends JFrame {
+public class Login extends JFrame {
 
 	private JPanel contentPane;
 
@@ -17,7 +17,7 @@ public class login extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					login frame = new login();
+					Login frame = new Login();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -29,7 +29,7 @@ public class login extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public login() {
+	public Login() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
