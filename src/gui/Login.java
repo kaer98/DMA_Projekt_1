434d0c590ad35d6020b84f689dea018e22244084
DePlaced <117630280+DePlaced@users.noreply.github.com>
2023-01-11 12:59:29 +0100

    Update Login.java

--- src/gui/Login.java
+++ src/gui/Login.java
@@ -23,11 +23,6 @@ import java.awt.event.MouseEvent;
 
 public class Login extends JFrame {
 
-<<<<<<< Updated upstream
-=======
-public class Login extends JFrame {
->>>>>>> Stashed changes
-
 	private JPanel contentPane;
 	private JPanel panel;
 	private JButton btnTest;
